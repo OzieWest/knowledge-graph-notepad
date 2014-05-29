d083f358cd7642abec7e5c582fc21cd00f2e2c2c
soramusoka <soramusoka@gmail.com>
2014-05-29 23:53:53 +0400

    added Select2 for lasy loading categories

--- KnowledgeGraph/ngApp/app.directives.js
+++ KnowledgeGraph/ngApp/app.directives.js
@@ -13,7 +13,8 @@
 			templateUrl: '../templates/tmpl.directive.new-topic.html?1',
 			controller: function ($scope, topicRepository) {
 				var that = this;
-
+				$scope.currentCategory = {};
+				
 				$scope.newTopicId = 0;
 				$scope.isView = {
 					NewTopic: false,
@@ -51,10 +52,15 @@
 				};
 
 				that.checkModel = function () {
-					
+					if (!$scope.currentCategory.text) {
+						return false;
+					} else {
+						$scope.model.Category = $scope.currentCategory.text;
+					}
+
 					$scope.model.Connections.push($scope.parent.Id);
 
-					if ($scope.model.Title && $scope.model.Category && $scope.model.Value) {
+					if ($scope.model.Title && $scope.model.Value) {
 						return true;
 					}
 
@@ -96,6 +102,44 @@
 				// INIT -----------------------------------------------
 				that.clearTopic();
 				that.clearLink();
+			}
+		};
+	});
+	
+	app.directive('categoryList', function () {
+		return {
+			replace: true,
+			restrict: 'E',
+			scope: {
+				model: "=",
+			},
+			template: '<input type="text" ui-select2="categoryOptions" ng-model="model">',
+			controller: function ($scope) {
+				var that = this;
+				
+				$scope.categories = [
+					{ id: 0, text: "Общее" },
+					{ id: 1, text: "Алгоритмы" },
+					{ id: 2, text: "Биология" },
+					{ id: 3, text: "Биография" },
+					{ id: 4, text: "Математика" },
+					{ id: 5, text: "История" },
+					{ id: 6, text: "Физика" },
+				];
+
+				$scope.categoryOptions = {
+					minimumInputLength: 0,
+					placeholder: "Search category...",
+					width: 250,
+					query: function (query) {
+						//console.log(query.term);
+						var data = {};
+						data.results = $scope.categories;
+						query.callback(data);
+					},
+					initSelection: function (element, callback) {
+					}
+				};
 			}
 		};
 	});
