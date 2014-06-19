92268676e5d7821ee131e1fe71b1197dcb8f6e67
soramusoka <soramusoka@gmail.com>
2014-06-19 23:33:52 +0400

    some test with nodeJS

--- nodeJS-App/server.js
+++ nodeJS-App/server.js
@@ -1,26 +1,74 @@
-// 539d74cfeebbdcc40905b4e0
-
-var v = require('./topicValidator');
 var _ = require('underscore');
-var topicController = require('./dbfunc');
-
+var logger = require('morgan');
+var express = require('express');
+var v = require('./topicValidator');
+var repo = require('./topicApiController');
 var mongo = require('mongodb');
+//var fs = require('fs');
+
+//var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var host = '127.0.0.1';
 var port = mongo.Connection.DEFAULT_PORT;
 var db = new mongo.Db('for-test', new mongo.Server(host, port, {}));
 
-var getDate = function(){
-	return new Date().toLocaleString();
-};
+repo.setDb(db);
+repo.setCollection('topic');
 
-var createId = function(stringKey){
-	return new mongo.ObjectID(stringKey);
-}
+var app = express();
 
-topicController.setDb(db);
-topicController.setCollection('topic');
+app.use(express.static(__dirname + '/public'));
+app.use(logger());
 
-var id = createId('539d74cfeebbdcc40905b4e0');
+
+app.get('/api/topics', function(req, resp){
+	var query = req.query;
+
+	var count = query.count || 10;
+	var offset = query.offset || 0;
+	var category = query.category || '';
+
+	var fields = getFields(query.fields);
+
+	repo.getAll(count, offset, fields, category, function(error, array) {
+		db.close();
+		if(error)
+			resp.send(error);
+		else
+			resp.send(array);
+	});
+});
+
+app.get('/api/topics/:id', function(req, resp){
+	var strId = req.params.id || '';
+	if(strId) {
+		var id = createId(strId);
+		var fields = getFields(req.query.fields);
+
+		repo.getById(id, fields, function(error, array) {
+			db.close();
+			if(error)
+				resp.send(error);
+			else
+				resp.send(array);
+		});
+	}
+	else
+		resp.send('Bad response');
+});
+
+app.get('*', function(req, resp){
+	var body = 'Default request!';
+	resp.setHeader('Content-Type', 'text/html');
+	resp.setHeader('Content-Length', body.length);
+	resp.send(body);
+});
+app.listen(3000);
+console.log('Server is running...');
+
+//var id = createId('539d74cfeebbdcc40905b4e0');
+
+/*var id = createId('539d74cfeebbdcc40905b4e0');
 var secondId = createId('539d902a5d2de1182034dd39');
 var thirdId = createId('539d902a5d2de1182034dd37');
 
@@ -34,7 +82,7 @@ var topic = {
 	modified: getDate(),
 };
 
-/*topicController.getAll(5, 0, {}, '', function(err, data) {
+repo.getAll(5, 0, {}, '', function(err, data) {
 
 	_.each(data, function(elm){
 		console.log(elm);
@@ -42,55 +90,71 @@ var topic = {
 	db.close();
 });*/
 
-/*topicController.getById(id, { title: 1 }, function(error, topic){
+/*repo.getById(id, {}, function(error, topic){
 	console.log(topic);
 
 	db.close();
 });*/
 
-/*topicController.addTopic(topic, function(error){
+/*repo.addTopic(topic, function(error){
 	console.log(error);
 	db.close();
 });*/
 
-/*topicController.updateTopic(id, topic, function(error, result) {
+/*repo.updateTopic(id, topic, function(error, result) {
 	console.log(error, result);
 	db.close();
 });*/
 
-/*topicController.deleteTopic(deleteId, function(error, result){
+/*repo.deleteTopic(deleteId, function(error, result){
 	console.log(error, result);
 	db.close();
 });*/
 
-/*topicController.getTitles([id, createId('539d902a5d2de1182034dd37')], { title: 1 }, function(error, result){
+/*repo.getTitles([id, createId('539d902a5d2de1182034dd37')], { title: 1 }, function(error, result){
 	console.log(error, result);
 	db.close();
 });*/
 
-/*topicController.searchTitle('sea', { title: 1 }, function(error, result) {
+/*repo.searchTitle('sea', { title: 1 }, function(error, result) {
 	console.log(error, result);
 	db.close();
 });*/
 
-/*topicController.addConnection(id, thirdId, function(error) {
+/*repo.addConnection(id, thirdId, function(error) {
 	console.log(error);
 	db.close();
 });*/
 
-/*topicController.deleteConnection(id, thirdId, function(error) {
+/*repo.deleteConnection(id, thirdId, function(error) {
 	console.log(error);
 	db.close();
 });*/
 
-var link = { id: 1, type: 'в ожидании', title: 'вики', url: 'http://google.com' };
-/*topicController.addLink(id, link, function(error) {
+//var link = { id: 1, type: 'в ожидании', title: 'вики', url: 'http://google.com' };
+/*repo.addLink(id, link, function(error) {
 	console.log(error);
 	db.close();
 });*/
 
-/*topicController.deleteLink(id, 1, function(error) {
+/*repo.deleteLink(id, 1, function(error) {
 	console.log(error);
 	db.close();
 });*/
 
+var getDate = function(){
+	return new Date().toLocaleString();
+};
+
+var createId = function(stringKey){
+	return new mongo.ObjectID(stringKey);
+};
+
+var getFields = function(str) {
+	var tempFields = (str || '').split('.');
+	var fields = {};
+	_.each(tempFields, function(e) {
+		fields[e] = 1;
+	});
+	return fields;
+};
